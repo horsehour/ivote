963c5a5563abe1b442cb62a591645558db724801
jiangc4 <jiangchunheng@gmail.com>
2017-03-18 02:03:02 -0400

    add VoteTrain, updates others

--- src/com/horsehour/vote/rule/LearnedRule.java
+++ src/com/horsehour/vote/rule/LearnedRule.java
@@ -2,27 +2,38 @@ package com.horsehour.vote.rule;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import com.horsehour.vote.Profile;
 
 /**
- *
  * @author Chunheng Jiang
  * @version 1.0
  * @since 8:40:03 PM, Jun 26, 2016
- *
  */
 
 public class LearnedRule extends VotingRule implements Serializable {
 	private static final long serialVersionUID = -7649740770312574806L;
+	/**
+	 * Abstract election mechanism
+	 */
 	Function<Profile<?>, List<?>> mechanism;
+
+	/**
+	 * Abstract election agent with input profile and alternatives
+	 */
+	BiFunction<Profile<Integer>, List<Integer>, List<Integer>> agent;
 
 	String name = "";
 
 	@SuppressWarnings("unchecked")
 	public <T> LearnedRule(Function<Profile<T>, List<T>> mechanism) {
 		this.mechanism = profile -> mechanism.apply((Profile<T>) profile);
+	}
+
+	public LearnedRule(BiFunction<Profile<Integer>, List<Integer>, List<Integer>> agent) {
+		this.agent = (profile, items) -> agent.apply(profile, items);
 	}
 
 	@Override
@@ -33,6 +44,10 @@ public class LearnedRule extends VotingRule implements Serializable {
 	@SuppressWarnings("unchecked")
 	public <T> List<T> getAllWinners(Profile<T> profile) {
 		return (List<T>) mechanism.apply(profile);
+	}
+
+	public List<Integer> getAllWinners(Profile<Integer> profile, List<Integer> items) {
+		return agent.apply(profile, items);
 	}
 
 	public void setName(String name) {
---
+++ src/com/horsehour/vote/train/VoteTrain.java
@@ -0,0 +1,31 @@
+package com.horsehour.vote.train;
+
+import java.util.List;
+
+import com.horsehour.vote.Profile;
+
+/**
+ * @author Chunheng Jiang
+ * @since Mar 18, 2017
+ * @version 1.0
+ */
+public class VoteTrain {
+	/**
+	 * Generate training profiles with specific parameters
+	 * 
+	 * @param m
+	 *            number of alternatives
+	 * @param n
+	 *            number of voters
+	 * @param k
+	 *            number of profiles
+	 * @return Training profiles
+	 */
+	public List<Profile<Integer>> generateTrainProfiles(int m, int n, int k) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
